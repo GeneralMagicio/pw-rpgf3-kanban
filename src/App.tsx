7e6cc0c3e68fb90876dd42157ab24683fe2c049f
Kristofer <kristofer@fmckl.se>
2023-10-26 17:35:16 +0200

    Switch applicantType

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,6 @@ import { useState, useEffect, DragEvent } from "react";
 
 import fs from "vite-plugin-fs/browser";
 
-type Application = {
-  applicantAddress: string;
-  displayName: string;
-  pwCategory?: string;
-  contributionDescription: string;
-  impactDescription: string;
-  websiteUrl: string;
-  pwIsFlagged?: boolean;
-  pwFlaggedReason?: string;
-};
-
 const ProjectCategories = [
   "AI, Artificial Intelligence & Security",
   "Asset Management & Portfolio Tools",
@@ -75,6 +64,57 @@ const individualCategories = [
   "Partnerships & Collaborations",
   "Specialized & Niche Contributions",
 ];
+
+interface ContributionLink {
+  type: string;
+  url: string;
+  description: string;
+}
+
+interface ImpactMetric {
+  description: string;
+  number: number;
+  url: string;
+}
+
+interface FundingSource {
+  type: string;
+  currency: string;
+  amount: number;
+  description: string;
+}
+
+interface Application {
+  applicantType: string;
+  websiteUrl: string;
+  bio: string;
+  contributionDescription: string;
+  contributionLinks: ContributionLink[];
+  impactCategory: string[];
+  impactDescription: string;
+  impactMetrics: ImpactMetric[];
+  fundingSources: FundingSource[];
+  payoutAddress: string;
+  understoodKYCRequirements: boolean;
+  understoodFundClaimPeriod: boolean;
+  certifiedNotDesignatedOrSanctionedOrBlocked: boolean;
+  certifiedNotSponsoredByPoliticalFigureOrGovernmentEntity: boolean;
+  certifiedNotBarredFromParticipating: boolean;
+  displayName: string;
+  applicantName: string;
+  profileImageUrl: string;
+  bannerImageUrl: string;
+  applicationMetadataPtr: string;
+  RPGF3_Application_UID: string;
+  applicantAddress: string;
+  pwIsFlagged?: boolean;
+  pwApplicantTypeChecked?: boolean;
+  pwCategorySuggestions?: string;
+  pwCategory?: string;
+  pwFlaggedReason?: string;
+  pwRecategorizeToType?: string;
+  pwRecategorizeReason?: string;
+}
 
 type ApplicationType = "PROJECT" | "INDIVIDUAL";
 
@@ -142,15 +182,34 @@ export default function App() {
       closePopup();
     }
   };
-  const handleDrop = (e: DragEvent, category: string) => {
+  const handleDrop = (e: DragEvent, category: string | undefined) => {
     e.preventDefault();
     const appId = e.dataTransfer.getData("appId");
     const app = applications.find((app) => app.applicantAddress === appId);
     if (app) {
-      app.pwCategory = category;
+      if (category === undefined) {
+        delete app.pwCategory;
+      } else {
+        app.pwCategory = category;
+      }
       saveApp(app);
       setApplications([...applications]);
     }
+  };
+
+  const handleSwitchApplicantType = async (app: Application) => {
+    if (app.applicantType === "PROJECT") {
+      app.applicantType = "INDIVIDUAL";
+    } else {
+      app.applicantType = "PROJECT";
+    }
+    delete app.pwCategory;
+    await saveApp(app);
+    setApplications(
+      applications.map((a) =>
+        a.applicantAddress === app.applicantAddress ? app : a
+      )
+    );
   };
 
   const handleClick = (app: Application) => {
@@ -178,6 +237,38 @@ export default function App() {
         </button>
       </div>
       <div className="flex">
+        <div
+          className="flex-1 p-4"
+          key={undefined}
+          onDrop={(e) => handleDrop(e, undefined)}
+          onDragOver={(e) => e.preventDefault()}
+        >
+          {" "}
+          <h2 className="mb-4 text-2xl">Uncategorized</h2>
+          {applications
+            .filter(
+              (app) =>
+                !app.pwCategory &&
+                app.applicantType === applicationType &&
+                !app.pwIsFlagged
+            )
+            .map((app) => (
+              <div
+                className="p-2 mb-2 bg-orange-800 border cursor-pointer"
+                key={app.applicantAddress}
+                draggable
+                onDragStart={(e) => handleDragStart(e, app.applicantAddress)}
+                onClick={() => handleClick(app)}
+              >
+                <img
+                  src={app.profileImageUrl}
+                  className="w-10 h-10 rounded-full"
+                  loading="lazy"
+                />
+                {app.displayName}
+              </div>
+            ))}
+        </div>
         {categories.map((category) => (
           <div
             className="flex-1 p-4"
@@ -196,6 +287,11 @@ export default function App() {
                   onDragStart={(e) => handleDragStart(e, app.applicantAddress)}
                   onClick={() => handleClick(app)}
                 >
+                  <img
+                    src={app.profileImageUrl}
+                    className="w-10 h-10 rounded-full"
+                    loading="lazy"
+                  />
                   {app.displayName}
                 </div>
               ))}
@@ -203,11 +299,16 @@ export default function App() {
         ))}
       </div>
       {selectedApp && (
-        <div className="fixed top-0 left-0 flex items-center justify-center w-full h-full bg-gray-500 bg-opacity-50">
-          <div className="max-w-2xl p-4 text-white bg-black rounded">
-            <button className="float-right" onClick={closePopup}>
+        <div className="fixed top-0 left-0 flex flex-col items-center justify-center w-full h-full gap-2 bg-gray-500 bg-opacity-50">
+          <div className="w-[700px] text-right">
+            <button
+              className="px-4 py-2 rounded-full bg-slate-400"
+              onClick={closePopup}
+            >
               X
             </button>
+          </div>
+          <div className="w-[700px] p-5 overflow-scroll text-white bg-black rounded max-h-[600px]">
             <div className="flex flex-col gap-5">
               <h2>{selectedApp.displayName}</h2>
               <p>{selectedApp.contributionDescription}</p>
@@ -217,24 +318,58 @@ export default function App() {
                   {selectedApp.websiteUrl}
                 </a>
               </p>
-            </div>
-            <div className="mt-4">
-              <h2 className="text-xl">Flag this application</h2>
-              <label htmlFor="flagReason" className="block mt-2">
-                Flag reason
-              </label>
-              <textarea
-                id="flagReason"
-                value={flagReason}
-                onChange={(e) => setFlagReason(e.target.value)}
-                className="w-full p-2 mt-1 text-black border rounded bg-slate-400"
-              ></textarea>
-              <button
-                onClick={handleFlag}
-                className="p-2 mt-2 text-white bg-red-600 rounded"
-              >
-                Flag
-              </button>
+              <h2 className="text-xl">Contribution links</h2>
+              {selectedApp.contributionLinks.map((link) => (
+                <div key={link.url}>
+                  <a href={link.url} target="_blank">
+                    {link.type} - {link.description}
+                  </a>
+                </div>
+              ))}
+              <h2 className="text-xl">Impact metrics</h2>
+              {selectedApp.impactMetrics.map((metric) => (
+                <div key={metric.url}>
+                  <a href={metric.url} target="_blank">
+                    {metric.description} - {metric.number}
+                  </a>
+                </div>
+              ))}
+              <h2 className="text-xl">Funding sources</h2>
+              {selectedApp.fundingSources.map((source) => (
+                <div key={source.description}>
+                  {source.type} - {source.currency} - {source.amount} -{" "}
+                  {source.description}
+                </div>
+              ))}
+              <div className="flex flex-col gap-2 p-5 bg-red-800">
+                <h2 className="text-xl">Switch applicant type</h2>
+                <button
+                  className="p-2 mt-2 border rounded"
+                  onClick={() => handleSwitchApplicantType(selectedApp)}
+                >
+                  {selectedApp.applicantType === "PROJECT"
+                    ? "Switch to individual"
+                    : "Switch to project"}
+                </button>
+                <h2 className="text-xl">Flag this application</h2>
+                <div>
+                  <label htmlFor="flagReason" className="block mt-2">
+                    Flag reason
+                  </label>
+                  <textarea
+                    id="flagReason"
+                    value={flagReason}
+                    onChange={(e) => setFlagReason(e.target.value)}
+                    className="w-full p-2 mt-1 bg-red-900 border rounded "
+                  ></textarea>
+                </div>
+                <button
+                  onClick={handleFlag}
+                  className="p-2 mt-2 border rounded"
+                >
+                  Flag
+                </button>
+              </div>
             </div>
           </div>
         </div>
